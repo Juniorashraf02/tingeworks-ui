a3aebdfb5d39d2a45ae6b5fb6f71d48926d2142c
juniorashraf02 <shuvo.istiak11@gmail.com>
2022-09-22 08:29:10 +0600

    responsive issue fixed for btn group

--- components/Buttons/ButtonGroup.tsx
+++ components/Buttons/ButtonGroup.tsx
@@ -1,6 +1,6 @@
 export default function ButtonGroup(props: any) {
 	return (
-		<div className='flex gap-2 items-center mx-auto container px-5 lg:px-10 my-5'>
+		<div className='flex gap-2 items-center mx-auto container px-5 lg:px-10 my-5 overflow-x-scroll scrollbar-hide '>
 			{props.children} 
 		</div>
 	);
