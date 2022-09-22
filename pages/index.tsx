5186bcc7c4a479013e5bc283a3edaedb67b554a9
juniorashraf02 <shuvo.istiak11@gmail.com>
2022-09-22 08:01:47 +0600

    index page modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import ButtonGroup from '../components/Buttons/ButtonGroup';
 import CTA from '../components/Buttons/CTA';
 import Contact from '../components/Contact/Contact';
 import Footer from '../components/Footer/Footer';
@@ -33,6 +34,9 @@ const Home: NextPage = () => {
 				</div>
 			</div>
 
+
+
+
 			{/* About section */}
 			<div className='my-12 mx-auto'>
 			  <p className=" text-gray-200 md:text-9xl text-4xl h-5 font-black uppercase text-center md:h-20">
@@ -49,6 +53,7 @@ const Home: NextPage = () => {
 			<br />
 			<p className='h-0'>brand identity digital storytelling, branding tools & importantly just love to create </p>
 			<br />
+
 			<p>beautiful, unique designs</p>
 			</div>
 
