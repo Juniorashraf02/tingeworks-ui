e9c81bdac692f33f6bf0634adba06581bef0accf
juniorashraf02 <shuvo.istiak11@gmail.com>
2022-09-22 07:57:25 +0600

    btn group addedmodified

--- components/Buttons/ButtonGroup.tsx
+++ components/Buttons/ButtonGroup.tsx
@@ -1,7 +1,7 @@
 export default function ButtonGroup(props: any) {
 	return (
 		<div className='flex gap-2 items-center mx-auto container px-5 lg:px-10 my-5'>
-			{props.children}
+			{props.children} 
 		</div>
 	);
 }
